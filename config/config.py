286908865d8781f649bfd62ace8106abe91b2f89
Patrick Riel <priel@cisco.com>
2020-01-11 17:23:22 -0500

    change url

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-CISCO_VISION_DMP_URL = 'http://10.1.201.21:8080/StadiumVision/ws/rest/trigger/input/21'
+CISCO_VISION_DMP_URL = 'http://10.1.201.21:8080/StadiumVision/ws/rest/trigger/input/5'
 MERAKI_MV_LINK = 'https://n190.meraki.com/CiscoNRF2020/n/Vr1OGa-c/manage/nodes/new_list/57548243929302/video?timespan=86400&timestamp='
 
 MQTT_BROKER = '10.1.201.226'
